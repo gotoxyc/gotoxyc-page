848de4c14fabb22ae0d6af7a4207a1f6b29e7a70
gotoxyc <marycielorodriguezalc@gmail.com>
2023-12-10 19:20:44 -0600

    toggle dark mode

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -1,8 +1,11 @@
-import { Flex, Link as ChakraLink } from "@chakra-ui/react";
-
+import { Flex, IconButton, Link as ChakraLink } from "@chakra-ui/react";
+import { FaSun, FaMoon } from "react-icons/fa";
 import NextLink from "next/link";
+import { useColorMode } from "@chakra-ui/react";
 
 export function MenuBar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Flex align="center" marginBottom={8} justify="space-evenly" width="full">
       <ChakraLink as={NextLink} href="/">
@@ -20,6 +23,9 @@ export function MenuBar() {
       <ChakraLink as={NextLink} href="contact">
         contact
       </ChakraLink>
+      <IconButton onClick={toggleColorMode} variant="ghost">
+        {colorMode === "dark" ? <FaSun /> : <FaMoon />}
+      </IconButton>
     </Flex>
   );
 }
